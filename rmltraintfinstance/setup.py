a9bb3f044393bdce08d647429697052ce2702e57
Carson Schubert <carson.schubert14@gmail.com>
2020-04-24 18:38:33 -0500

    For realgit add . Rename bbox plugin to new scheme. Convert instance plugin into new pip install scheme and depend on our fork of OD, not injected folders.

--- rmltraintfinstance/setup.py
+++ rmltraintfinstance/setup.py
@@ -1,16 +1,31 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-# def dependencies(file):
-#     with open(file) as f:
-#         return f.read().splitlines()
+# determine GPU or CPU install via env variable
+gpu = os.getenv('RMLTRAIN_BBOX_GPU')
+tensorflow_pkg = 'tensorflow==1.14.0' if not gpu else 'tensorflow-gpu==1.14.0'
 
 setup(
-    name='ravenML_tf_instance',
+    name='rmltraintfinstance',
     version='0.1',
-    description='Training plugin for ravenml',
-    packages=['ravenml_tf_instance'],
+    description='Tensorflow Instance Segmentation training plugin for ravenML',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.16.4',
+        'cython==0.29.13',
+        'object-detection @ https://github.com/autognc/object-detection/tarball/object-detection#egg=object-detection',
+        'absl-py==0.8.0',
+        'pycocotools-fix==2.0.0.1',
+        'matplotlib==3.1.1',
+        'contextlib2==0.5.5',
+        'pillow==6.1.0',
+        'lxml==4.4.0',
+        'jupyter==1.0.0',
+        'comet-ml==2.0.13',
+        tensorflow_pkg
+    ],
     entry_points='''
         [ravenml.plugins.train]
-        tf_instance=ravenml_tf_instance.core:tf_instance
+        tf_instance=rmltraintfinstance.core:tf_instance
     '''
 )
