8149eb391f51e6a122a6059c257f00bf6f9820b3
jacobDeutsch10 <jacobdeutsch@utexas.edu>
2020-06-14 00:09:40 -0500

    refactor of bbox into tunebbox and creation of Trainable class WORK IN PROGRESS

---
+++ rmltraintunetfbbox/scripts/recalculate_statistics_from_dump.py
@@ -0,0 +1,17 @@
+from rmltraintunetfbbox.validation.stats import BoundingBoxEvaluator
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dump_path')
+    parser.add_argument('output_path')
+    args = parser.parse_args()
+
+    evaluator = BoundingBoxEvaluator.load_from_dump(args.dump_path)
+    evaluator.calculate_default_and_save(args.output_path)
+    evaluator.dump(args.output_path + '/results.pickle')
+
+
+if __name__ == '__main__':
+    main()
