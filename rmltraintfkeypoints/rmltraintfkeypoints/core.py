Add correct principal point calculation and add comet logging of pose error during train

rmltraintfkeypoints/rmltraintfkeypoints/core.py
@@ -41,7 +41,7 @@ def train(ctx, train: TrainInput, config, comet):
 
     # set base directory for model artifacts
     artifact_dir = (LocalCache(global_cache.path / 'tf-keypoints').path if train.artifact_path is None \
-        else train.artifact_path) / 'artifacts'
+                        else train.artifact_path) / 'artifacts'
 
     if os.path.exists(artifact_dir):
         if user_confirms('Artifact storage location contains old data. Overwrite?'):
@@ -130,25 +130,29 @@ def eval(ctx, train, model_path, pnp_focal_length, plot=False, render_poses=Fals
     for image_batch, truth_batch in tqdm.tqdm(test_data):
         kps_batch = model(image_batch).numpy()
         kps_batch = kps_batch.reshape(kps_batch.shape[0], -1, 2)
-        # kps_pnp_batch = kps_batch * (truth_batch['bbox_size'] // 2)[:, None, None] + truth_batch['centroid'][:, None, :]
-        kps_crop_batch = kps_batch * (cropsize // 2) + (cropsize // 2)
+        kps_batch = kps_batch * (cropsize // 2) + (cropsize // 2)
         kps_true_batch = (truth_batch['keypoints'] - truth_batch['centroid'][:, None, :])\
             / truth_batch['bbox_size'][:, None, None] * cropsize + (cropsize // 2)
-        rescale_batch = cropsize / truth_batch['bbox_size']
-        for image, rescale, kps_crop, kps_true, pose in zip(image_batch.numpy(), rescale_batch.numpy(), kps_crop_batch,
-                                                            kps_true_batch.numpy(), truth_batch['pose'].numpy()):
-            image = ((image + 1) / 2 * 255).astype(np.uint8)
+        for i, (kps, kps_true) in enumerate(zip(kps_batch, kps_true_batch.numpy())):
+            image = ((image_batch[i].numpy() + 1) / 2 * 255).astype(np.uint8)
             r_vec, t_vec, cam_matrix, coefs = utils.calculate_pose_vectors(
-                ref_points, kps_crop,
-                pnp_focal_length, image.shape[:2],
-                rescale=rescale
+                ref_points, kps,
+                [pnp_focal_length, pnp_focal_length], image.shape[:2],
+                extra_crop_params={
+                    'centroid': truth_batch['centroid'][i],
+                    'bbox_size': truth_batch['bbox_size'][i],
+                    'imdims': truth_batch['imdims'][i],
+                }
             )
-            errs.append(utils.geodesic_error(r_vec, pose))
-            errs_by_keypoint.append([np.linalg.norm(kps_true[i] - kps_crop[i]) for i in range(len(kps_crop))])
+            errs.append(utils.geodesic_error(r_vec, truth_batch['pose'][i]))
+            errs_by_keypoint.append([
+                np.linalg.norm(kp_true - kp)
+                for kp, kp_true in zip(kps, kps_true)
+            ])
             if render_poses:
-                for kp_idx in range(len(kps_crop)):
-                    y = int(kps_crop[kp_idx, 0])
-                    x = int(kps_crop[kp_idx, 1])
+                for kp_idx in range(len(kps)):
+                    y = int(kps[kp_idx, 0])
+                    x = int(kps[kp_idx, 1])
                     ay = int(kps_true[kp_idx, 0])
                     ax = int(kps_true[kp_idx, 1])
                     cv2.circle(image, (x, y), 5, (255, 0, 255), -1)
@@ -165,7 +169,8 @@ def eval(ctx, train, model_path, pnp_focal_length, plot=False, render_poses=Fals
                 cv2.line(image, (p3[0], p3[1]), (p4[0], p4[1]), (255, 0, 255), 6)
                 cv2.line(image, (p1[0], p1[1]), (p4[0], p4[1]), (255, 0, 255), 6)
                 cv2.line(image, (p2[0], p2[1]), (p4[0], p4[1]), (255, 0, 255), 6)
-                cv2.imwrite(f'{train.artifact_path}/pose-render-{img_cnt:04d}.png', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+                cv2.imwrite(f'{train.artifact_path}/pose-render-{img_cnt:04d}.png',
+                            cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
             img_cnt += 1
 
     np.save(f'{train.artifact_path}/pose_errs.npy', np.array(errs))
@@ -206,7 +211,7 @@ def evalpnp(ctx, train, keypoints, pnp_focal_length, swap_random_percent):
         # FIXME: don't hardcode image resolution
         r_vec, t_vec, cam_matrix, coefs = utils.calculate_pose_vectors(
             ref_points[:nb_keypoints], kps[:nb_keypoints],
-            pnp_focal_length, [1024, 1024])
+            [pnp_focal_length, pnp_focal_length], [1024, 1024])
         err = utils.geodesic_error(r_vec, pose)
         errs.append(err)
 

rmltraintfkeypoints/rmltraintfkeypoints/data_utils.py
@@ -57,7 +57,8 @@ def dataset_from_directory(dir_path, cropsize, nb_keypoints):
             'keypoints': keypoints,
             'pose': tf.ensure_shape(metadata['pose'], [4]),
             'bbox_size': tf.ensure_shape(bbox_size, []),
-            'centroid': tf.ensure_shape(centroid, [2])
+            'centroid': tf.ensure_shape(centroid, [2]),
+            'imdims': imdims
         }
         return image, truth
 

rmltraintfkeypoints/rmltraintfkeypoints/train.py
@@ -5,15 +5,17 @@ from . import utils
 
 
 class PoseErrorCallback(tf.keras.callbacks.Callback):
-    def __init__(self, ref_points, cropsize, focal_length):
+    def __init__(self, ref_points, cropsize, focal_length, experiment=None):
         super().__init__()
         self.ref_points = ref_points
         self.crop_size = cropsize
         self.focal_length = focal_length
+        self.experiment = experiment
         self.targets = tf.Variable(0.0, shape=tf.TensorShape(None))
         self.outputs = tf.Variable(0.0, shape=tf.TensorShape(None))
         self.train_errors = []
         self.test_errors = []
+        self.comet_step = 0
 
     def assign_metric_ignore(self, y_true, y_pred):
         self.targets.assign(y_true)
@@ -25,13 +27,16 @@ class PoseErrorCallback(tf.keras.callbacks.Callback):
         kps_batch = self.outputs.numpy()
         kps_batch = kps_batch.reshape(kps_batch.shape[0], -1, 2)
         kps_batch = kps_batch * (self.crop_size // 2) + (self.crop_size // 2)
-        rescale_batch = self.crop_size / label['bbox_size']
         error_batch = np.zeros(kps_batch.shape[0])
         for i, kps in enumerate(kps_batch):
             r_vec, t_vec, _, _ = utils.calculate_pose_vectors(
                 self.ref_points, kps,
-                self.focal_length, [label['height'][i], label['width'][i]],
-                rescale_batch[i]
+                [self.focal_length, self.focal_length], [self.crop_size, self.crop_size],
+                extra_crop_params={
+                    'centroid': label['centroid'][i],
+                    'bbox_size': label['bbox_size'][i],
+                    'imdims': [label['height'][i], label['width'][i]],
+                }
             )
 
             error_batch[i] = utils.geodesic_error(r_vec, label['pose'][i].numpy())
@@ -41,18 +46,26 @@ class PoseErrorCallback(tf.keras.callbacks.Callback):
         self.train_errors = []
 
     def on_train_batch_end(self, batch, logs=None):
+        self.comet_step += 1
         self.train_errors.append(np.mean(self.calc_pose_error()))
         mean = np.mean(self.train_errors)
         print(f' - pose error: {mean:.4f} ({np.degrees(mean):.2f} deg)')
+        if self.experiment:
+            self.experiment.log_metric('pose_error_deg', np.degrees(mean), step=self.comet_step)
 
     def on_test_begin(self, logs=None):
         self.test_errors = []
 
     def on_test_batch_end(self, batch, logs=None):
+        self.comet_step += 1
         self.test_errors.append(np.mean(self.calc_pose_error()))
 
     def on_test_end(self, logs=None):
-        print('Eval pose error: ', np.mean(self.test_errors))
+        mean = np.mean(self.test_errors)
+        print(f'Eval pose error: {mean:.4f} ({np.degrees(mean):.2f} deg)')
+        if self.experiment:
+            with self.experiment.validate():
+                self.experiment.log_metric('pose_error_deg', np.degrees(mean), step=self.comet_step)
 
 
 class KeypointsModel:
@@ -187,7 +200,7 @@ class KeypointsModel:
         # cv2.imwrite('test.png', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
         model_path = os.path.join(logdir, "model.h5")
-        pose_error_callback = PoseErrorCallback(self.keypoints_3d, self.crop_size, self.hp['pnp_focal_length'])
+        pose_error_callback = PoseErrorCallback(self.keypoints_3d, self.crop_size, self.hp['pnp_focal_length'], experiment)
         callbacks = [
             tf.keras.callbacks.TensorBoard(
                 log_dir=logdir,

rmltraintfkeypoints/rmltraintfkeypoints/utils.py
@@ -3,18 +3,32 @@ import numpy as np
 import cv2
 
 
-def calculate_pose_vectors(ref_points, keypoints, focal_length, imdims,
-                           rescale=1.0):
+def calculate_pose_vectors(ref_points, keypoints, focal_length, imdims, extra_crop_params=None):
     """
     Calculates pose vectors using CV2's solvePNP.
+    :param ref_points: 3D reference points (Nx3 array)
+    :param keypoints: 2D image keypoints in (y, x) pixel coordinates (Nx2 array)
+    :param focal_length: original camera focal length (vertical, horizontal)
+    :param imdims: (height, width) current image dimensions
+    :param extra_crop_params: a optional dict with extra parameters that are necessary to
+        adjust for cropping and rescaling. If provided, must have the keys {'centroid', 'bbox_size', 'imdims'}
+        where 'imdims' are the original image dimensions before cropping/rescaling.
     """
     dist_coeffs = np.zeros((5, 1), dtype=np.float32)
     imdims = np.array(imdims)
-    fy, fx = imdims // 2 * rescale
-    focal_length *= rescale
+    if extra_crop_params:
+        assert extra_crop_params.keys() == {'centroid', 'bbox_size', 'imdims'}
+        original_imdims = np.array(extra_crop_params['imdims'])
+        origin = np.array(extra_crop_params['centroid']) - extra_crop_params['bbox_size'] / 2
+        center = original_imdims / 2 - origin
+        focal_length *= imdims / extra_crop_params['bbox_size']
+        center *= imdims / extra_crop_params['bbox_size']
+    else:
+        center = imdims / 2
+
     cam_matrix = np.array([
-        [focal_length, 0, fx],
-        [0, focal_length, fy],
+        [focal_length[1], 0, center[1]],
+        [0, focal_length[0], center[0]],
         [0, 0, 1]
     ], dtype=np.float32)
     keypoints = keypoints.copy()