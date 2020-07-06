423b0b9e1a45823f09b9b2e6ae5b9d46a439530f
Kevin Black <kevin3.black@gmail.com>
2020-07-05 19:55:09 -0500

    Fix keypoints rescaling for training

--- rmltraintfkeypoints/rmltraintfkeypoints/train.py
+++ rmltraintfkeypoints/rmltraintfkeypoints/train.py
@@ -579,6 +579,7 @@ class KeypointsModel:
             return x_coords, y_coords
         if keypoints_mode == 'coords':
             keypoints = tf.reshape(parsed_kps, [-1, 2])[:nb_keypoints]
+            keypoints *= img_size
             keypoints = (keypoints - centroid) / (bbox_size / 2)
             return tf.reshape(keypoints, [nb_keypoints * 2])
         elif keypoints_mode == 'mask':
