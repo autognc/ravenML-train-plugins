2d5976821fdf5051c9f208de1f9164dc2188c76c
Daniel Fonseka <dlfonseka@utexas.edu>
2020-08-05 15:10:35 -0500

    Cleans up training loop and associated functions

--- rmltraintfbbox/rmltraintfbbox/core.py
+++ rmltraintfbbox/rmltraintfbbox/core.py
@@ -29,7 +29,7 @@ from ravenml.train.interfaces import TrainInput, TrainOutput
 from ravenml.utils.question import cli_spinner, user_selects, user_input
 from ravenml.utils.plugins import raise_parameter_error
 from rmltraintfbbox.utils.helpers import prepare_for_training, download_model_arch
-from rmltraintfbbox.validation.model import BoundingBoxModel
+from rmltraintfbbox.utils.exporter import export_inference_graph
 from rmltraintfbbox.validation.stats import BoundingBoxEvaluator
 from google.protobuf import text_format
 from matplotlib import pyplot as plt
@@ -57,7 +57,6 @@ def train(ctx: click.Context, train: TrainInput):
     # the user did not pass a config. see ravenml core file train/commands.py for more detail
     
     # NOTE: after training, you must create an instance of TrainOutput and return it
-    
     # import necessary libraries
     cli_spinner("Importing TensorFlow...", _import_od)
 
@@ -65,6 +64,9 @@ def train(ctx: click.Context, train: TrainInput):
     config = train.plugin_config
     metadata = train.plugin_metadata
     comet = config.get('comet')
+
+    if not config['verbose']:
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
     
     # set base directory for model artifacts 
     base_dir = train.artifact_path
@@ -102,6 +104,8 @@ def train(ctx: click.Context, train: TrainInput):
         ctx.exit('Training cancelled.')
 
     model_dir = os.path.join(base_dir, 'models/model')
+    train_dir = os.path.join(model_dir, 'train')
+    eval_dir = os.path.join(model_dir, 'eval')
     pipeline_config_path = os.path.join(model_dir, 'pipeline.config')
 
     experiment = None
@@ -115,8 +119,7 @@ def train(ctx: click.Context, train: TrainInput):
         experiment.log_asset(pipeline_config_path)
 
     # get number of training steps
-    num_train_steps = metadata['hyperparameters']['train_steps']
-    num_train_steps = int(num_train_steps)
+    num_train_steps = int(metadata['hyperparameters']['train_steps'])
 
     configs = config_util.get_configs_from_pipeline_file(pipeline_config_path)
     model_config = configs['model']
@@ -128,11 +131,14 @@ def train(ctx: click.Context, train: TrainInput):
     detection_model = model_builder.build(model_config=model_config, is_training=True)
     
     #create tf.data.Dataset()
-    train_input = inputs.train_input(train_config, train_input_config, model_config, detection_model)
+    train_input = inputs.train_input(train_config, train_input_config, model_config, model=detection_model)
+    eval_input = inputs.eval_input(eval_config, eval_input_config, model_config, model=detection_model)
+
+    train_input_iterator = iter(train_input)
 
     global_step = tf.Variable(
-        0, trainable=False, dtype=tf.compat.v2.dtypes.int64, name='global_step',
-        aggregation=tf.compat.v2.VariableAggregation.ONLY_FIRST_REPLICA)
+        0, trainable=False, dtype=tf.int64, name='global_step',
+        aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA)
     optimizer, (learning_rate,) = od.builders.optimizer_builder.build(
         train_config.optimizer, global_step=global_step)
 
@@ -140,124 +146,73 @@ def train(ctx: click.Context, train: TrainInput):
       learning_rate_fn = learning_rate
     else:
       learning_rate_fn = lambda: learning_rate
-    features, labels = iter(train_input).next()
+
+    #restore from checkpoint
+    load_fine_tune_checkpoint(detection_model, train_config.fine_tune_checkpoint, 
+                                        train_config.fine_tune_checkpoint_type,
+                                        train_config.fine_tune_checkpoint_version,
+                                        train_input, 
+                                        train_config.unpad_groundtruth_tensors)
 
     @tf.function
-    def _dummy_computation_fn(features, labels):
-        detection_model._is_training = False  # pylint: disable=protected-access
-        tf.keras.backend.set_learning_phase(False)
+    def train_step(detection_model, train_input_iterator, optimizer, learning_rate_fn, global_step):
 
-        labels = model_lib.unstack_batch(
-            labels, unpad_groundtruth_tensors=train_config.unpad_groundtruth_tensors)
+        features, labels = train_input_iterator.next()
+        loss = model_lib2.eager_train_step(detection_model, features,
+                            labels, train_config.unpad_groundtruth_tensors,
+                            optimizer, learning_rate_fn(),
+                            add_regularization_loss=True,
+                            clip_gradients_value=None,
+                            global_step=global_step)
+        global_step.assign_add(1)
+        return loss
 
-        return model_lib2._compute_losses_and_predictions_dicts(
-            detection_model,
-            features,
-            labels)
+    #@tf.function
+    def evaluate(detection_model, configs, eval_input, global_step):
 
-    _dummy_computation_fn(features,labels)
-    restore_from_objects_dict = detection_model .restore_from_objects(
-        fine_tune_checkpoint_type=train_config.fine_tune_checkpoint_type)
-    #validate_tf_v2_checkpoint_restore_map(restore_from_objects_dict)
-    ckpt = tf.train.Checkpoint(**restore_from_objects_dict)
-    ckpt.restore(train_config.fine_tune_checkpoint).expect_partial()
+        text_trap = io.StringIO()
+        sys.stdout = text_trap
+        sys.stderr = text_trap
+        
+        metrics = model_lib2.eager_eval_loop(detection_model, configs, eval_input, global_step=global_step)
+        
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
 
-    ckpt = tf.train.Checkpoint(step=global_step, model=detection_model, optimizer=optimizer)
-    checkpoint_max_to_keep = 2 #add this to arg of function?
+        click.echo(f'Evaluation loss: {metrics["Loss/total_loss"]}, Evaluation mAP: {metrics["DetectionBoxes_Precision/mAP"]}')
 
-    manager = tf.train.CheckpointManager(ckpt, model_dir, max_to_keep=checkpoint_max_to_keep)
-    latest_checkpoint = tf.train.latest_checkpoint(model_dir)
-    ckpt.restore(latest_checkpoint)
+        return metrics
 
-    # actually train
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+    manager = tf.train.CheckpointManager(checkpoint, directory=model_dir, max_to_keep=5)
+      
     with ExitStack() as stack:
         if comet:
             stack.enter_context(experiment.train())
         click.echo('Training model...')
 
-        num_steps_per_iteration = 1
+        start = time.time()
+        #main training loop
+        for step in range(1, num_train_steps+1):
 
-        clip_gradients_value = None
-        if train_config.gradient_clipping_by_norm > 0:
-            clip_gradients_value = train_config.gradient_clipping_by_norm
-        def train_step_fn(features, labels):
-            """Single train step."""
-            loss = model_lib2.eager_train_step(
-                detection_model,
-                features,
-                labels,
-                train_config.unpad_groundtruth_tensors,
-                optimizer,
-                learning_rate=learning_rate_fn(),
-                add_regularization_loss=train_config.add_regularization_loss,
-                clip_gradients_value=clip_gradients_value,
-                global_step=global_step)
-            global_step.assign_add(1)
-            return loss
+            loss = train_step(detection_model, train_input_iterator, optimizer, learning_rate_fn, global_step)
+            
+            if step % 10 == 0:
+                print(f'Training loss at step {step}: {loss}')
 
-        def _sample_and_train(train_step_fn, data_iterator):
-            features, labels = data_iterator.next()
-            per_replica_losses = train_step_fn(features, labels)
-            # TODO(anjalisridhar): explore if it is safe to remove the
-            ## num_replicas scaling of the loss and switch this to a ReduceOp.Mean
-            return per_replica_losses
-
-        @tf.function
-        def _dist_train_step(data_iterator):
-            """A distributed train step."""
-
-            if num_steps_per_iteration > 1:
-                for _ in tf.range(num_steps_per_iteration - 1):
-                    _sample_and_train(jtrain_step_fn, data_iterator)
-            return _sample_and_train(train_step_fn, data_iterator)
-        train_input_iter = iter(train_input)
-
-        if int(global_step.value()) == 0:
-            manager.save()
-
-        checkpointed_step = int(global_step.value())
-        logged_step = global_step.value()
-
-        last_step_time = time.time()
-        checkpoint_every_n = 10
-        #TODO: implement eval and comet-opt in main train loop
-        for _ in range(global_step.value(), num_train_steps,
-                        num_steps_per_iteration):
-
-            loss = _dist_train_step(train_input_iter)
-
-            time_taken = time.time() - last_step_time
-            last_step_time = time.time()
-
-            tf.compat.v2.summary.scalar(
-                'steps_per_sec', num_steps_per_iteration * 1.0 / time_taken,
-                step=global_step)
-
-            if global_step.value() - logged_step >= 1000:
-                print(
-                    'Step {} per-step time {:.3f}s loss={:.3f}'.format(
-                        global_step.value(), time_taken / num_steps_per_iteration,
-                        loss))
-                logged_step = global_step.value()
-
-            if ((int(global_step.value()) - checkpointed_step) >= checkpoint_every_n):
+            if step % 20 == 0:
+                evaluate(detection_model, configs, eval_input, global_step)
                 manager.save()
-                checkpointed_step = int(global_step.value())
 
-
-        #tf.estimator.train_and_evaluate(estimator, train_spec, eval_specs[0])
-        click.echo('Training complete')
+        training_time = time.time() - start
+        
+        click.echo(f'Training complete. Took {training_time} seconds.')
 
     # final metadata and return of TrainOutput object
     metadata['date_completed_at'] = datetime.utcnow().isoformat() + "Z"
 
     # get extra config files
-    extra_files = _get_paths_for_extra_files(base_dir)
-
-    #export detection_model as SavedModel
-    saved_model_path = model_dir + '/saved_model'
-    os.mkdir(saved_model_path)
-    tf.saved_model.save(detection_model, str(saved_model_path))
+    extra_files, final_ckpt = _get_paths_for_extra_files(base_dir)
 
     if comet:
         experiment.log_asset(model_path)
@@ -277,15 +232,19 @@ def train(ctx: click.Context, train: TrainInput):
             image_dataset = utils.get_image_dataset(test_path)
             truth_data = list(utils.gen_truth_data(test_path))
 
-            evaluator = BoundingBoxEvaluator(od.utils.label_map_util.create_category_index_from_labelmap(label_path))
+            category_index = od.utils.label_map_util.create_category_index_from_labelmap(label_path)
+            evaluator = BoundingBoxEvaluator(category_index)
 
             for (i, (bbox, centroid, z)), image in zip(enumerate(truth_data), image_dataset):
                 true_shape = tf.expand_dims(tf.convert_to_tensor(image.shape), axis=0)
                 truth = {'groundtruth_boxes': bbox, 'groundtruth_classes': 1}
                 start = time.time()
-                output = detection_model.call(tf.cast(tf.expand_dims(image, axis=0), dtype=tf.float32))
+                img, _ = detection_model.preprocess(tf.expand_dims(image, axis=0))
+                output = detection_model.call(tf.expand_dims(image, axis=0))
                 inference_time = time.time() - start
                 evaluator.add_single_result(output, true_shape, inference_time, bbox, centroid)
+                drawn_img = od.utils.visualization_utils.draw_bounding_boxes_on_image_tensors(tf.cast(tf.expand_dims(image, axis=0), dtype=tf.uint8), output['detection_boxes'], tf.cast(output['detection_classes'], dtype=tf.int32), output['detection_scores'], category_index, max_boxes_to_draw=1, min_score_thresh=0)
+                tf.keras.preprocessing.image.save_img(output_path+f'/img{i}.png', drawn_img[0])
 
             evaluator.dump(os.path.join(output_path, 'validation_results.pickle'))
             if comet:
@@ -299,7 +258,12 @@ def train(ctx: click.Context, train: TrainInput):
                 experiment.log_asset(os.path.join(output_path, 'stats.json'))
                 for img in glob.glob(os.path.join(output_path, '*_curve_*.png')):
                     experiment.log_image(img)
-    
+
+    #export detection_model as SavedModel
+    saved_model_path = model_dir + '/export'
+    configproto = config_util.create_pipeline_proto_from_configs(configs)
+    export_inference_graph('image_tensor', configproto, model_dir, saved_model_path)
+
     if comet:
         experiment.log_asset_data(train.metadata, file_name="metadata.json")
 
@@ -312,6 +276,75 @@ def train(ctx: click.Context, train: TrainInput):
     
 
 ### HELPERS ###
+
+def load_fine_tune_checkpoint(
+    model, checkpoint_path, checkpoint_type, checkpoint_version, input_dataset,
+    unpad_groundtruth_tensors):
+
+#NOTE: Moved this function from OD API to add 'expect_partial' to the restore
+  """Load a fine tuning classification or detection checkpoint.
+  To make sure the model variables are all built, this method first executes
+  the model by computing a dummy loss. (Models might not have built their
+  variables before their first execution)
+  It then loads an object-based classification or detection checkpoint.
+  This method updates the model in-place and does not return a value.
+  Args:
+    model: A DetectionModel (based on Keras) to load a fine-tuning
+      checkpoint for.
+    checkpoint_path: Directory with checkpoints file or path to checkpoint.
+    checkpoint_type: Whether to restore from a full detection
+      checkpoint (with compatible variable names) or to restore from a
+      classification checkpoint for initialization prior to training.
+      Valid values: `detection`, `classification`.
+    checkpoint_version: train_pb2.CheckpointVersion.V1 or V2 enum indicating
+      whether to load checkpoints in V1 style or V2 style.  In this binary
+      we only support V2 style (object-based) checkpoints.
+    input_dataset: The tf.data Dataset the model is being trained on. Needed
+      to get the shapes for the dummy loss computation.
+    unpad_groundtruth_tensors: A parameter passed to unstack_batch.
+  Raises:
+    IOError: if `checkpoint_path` does not point at a valid object-based
+      checkpoint
+    ValueError: if `checkpoint_version` is not train_pb2.CheckpointVersion.V2
+  """
+  if not model_lib2.is_object_based_checkpoint(checkpoint_path):
+    raise IOError('Checkpoint is expected to be an object-based checkpoint.')
+  if checkpoint_version == od.protos.train_pb2.CheckpointVersion.V1:
+    raise ValueError('Checkpoint version should be V2')
+
+  features, labels = iter(input_dataset).next()
+
+  @tf.function
+  def _dummy_computation_fn(features, labels):
+    model._is_training = False  # pylint: disable=protected-access
+    tf.keras.backend.set_learning_phase(False)
+    labels = model_lib.unstack_batch(
+        labels, unpad_groundtruth_tensors=unpad_groundtruth_tensors)
+
+    return model_lib2._compute_losses_and_predictions_dicts(
+        model,
+        features,
+        labels)
+
+  strategy = tf.compat.v2.distribute.get_strategy()
+  if hasattr(tf.distribute.Strategy, 'run'):
+    strategy.run(
+        _dummy_computation_fn, args=(
+            features,
+            labels,
+        ))
+  else:
+    strategy.experimental_run_v2(
+        _dummy_computation_fn, args=(
+            features,
+            labels,
+        ))
+
+  restore_from_objects_dict = model.restore_from_objects(
+      fine_tune_checkpoint_type=checkpoint_type)
+  ckpt = tf.train.Checkpoint(**restore_from_objects_dict)
+  ckpt.restore(checkpoint_path).expect_partial()
+
 def _get_paths_for_extra_files(artifact_path: Path):
     """Returns the filepaths for all checkpoint, config, and pbtxt (label)
     files in the artifact directory. Gets filepath for the exported inference
@@ -359,7 +392,7 @@ def _get_paths_for_extra_files(artifact_path: Path):
     #        extras.append(extras_path / 'eval_0' / f)
 
     extras.append(labels_path)
-    return extras
+    return extras, checkpoint_path
 
 # stdout redirection found at https://codingdose.info/2018/03/22/supress-print-output-in-python/
 def _import_od():
@@ -386,7 +419,7 @@ def _import_od():
     _dynamic_import('object_detection.model_lib', 'model_lib')
     _dynamic_import('object_detection.builders.model_builder', 'model_builder')
     _dynamic_import('object_detection.utils.config_util', 'config_util')
-    _dynamic_import('object_detection.exporter', 'exporter')
+    #_dynamic_import('object_detection.exporter_lib_v2', 'exporter')
     _dynamic_import('object_detection.inputs', 'inputs')
     _dynamic_import('object_detection', 'od')
     _dynamic_import('object_detection.protos', 'pipeline_pb2', asfunction=True)
