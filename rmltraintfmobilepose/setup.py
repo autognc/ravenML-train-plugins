8fc4f152851086c8572bb3cb19330825aa04ffb9
jacobDeutsch10 <47255678+jacobDeutsch10@users.noreply.github.com>
2021-10-17 23:09:50 -0500

    Update setup.py

--- rmltraintfmobilepose/setup.py
+++ rmltraintfmobilepose/setup.py
@@ -9,11 +9,12 @@ setup(
     packages=find_packages(),
     install_requires=[
         "numpy",  # ==1.18.4',
-        "tensorflow",  # ==2.1.0',
+        "tensorflow==2.3.0",
         "opencv-python",  # ==4.2.0.34',
         "tqdm",  # ==4.36.1',
         "matplotlib",  # ==2.2.2',
         "comet-ml",  # ==3.1.11',
+        "moderngl",
     ],
     entry_points="""
         [ravenml.plugins.train]
