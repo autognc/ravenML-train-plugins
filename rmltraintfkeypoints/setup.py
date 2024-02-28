10403956afb32c456b52e383dd265cb1bd3ca7f9
Troy Dutton <troydutton@utexas.edu>
2024-02-28 14:27:42 -0600

    Fixed keypoint training dependencies

--- rmltraintfkeypoints/setup.py
+++ rmltraintfkeypoints/setup.py
@@ -7,13 +7,14 @@ setup(
     description='Tensorflow keypoints regression plugin for ravenml',
     packages=find_packages(),
     install_requires=[
-        'numpy==1.18.4',
-        'tensorflow==2.1.0',
-        'opencv-python==4.2.0.34',
-        'tqdm==4.36.1',
-        'matplotlib==2.2.2',
-        'comet-ml==3.1.11',
-        'keras-unet==0.1.1'
+        'numpy',
+        'tensorflow',
+        'opencv-python',
+        'tqdm',
+        'matplotlib',
+        'comet-ml',
+        'keras-unet',
+        'keras_applications'
     ],
     entry_points='''
         [ravenml.plugins.train]
